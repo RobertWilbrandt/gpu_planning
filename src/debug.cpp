94d7a0f56b6a7a0c8b3e2e3032acfddf3a05d348
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-04-28 17:15:03 +0200

    Encapsulate bmp file writing

--- src/debug.cpp
+++ src/debug.cpp
@@ -72,6 +72,48 @@ uint32_t little_endian_32(uint32_t val) {
 #endif
 }
 
+void write_bmp(std::ostream& os, float* map, size_t width, size_t height) {
+  const size_t row_size = ((width * 3 - 1) / 4 + 1) * 4;
+
+  bmp_file_header file_header;
+  file_header.header_field = little_endian_16(0x4D42);
+  file_header.file_size =
+      little_endian_32(sizeof(bmp_file_header) + sizeof(bmp_dib_header_core) +
+                       height * row_size);
+  file_header.reserved_1 = 0;
+  file_header.reserved_2 = 0;
+  file_header.data_offset =
+      little_endian_32(sizeof(bmp_file_header) + sizeof(bmp_dib_header_core));
+  os.write(reinterpret_cast<char*>(&file_header), sizeof(file_header));
+
+  bmp_dib_header_core dib_header;
+  dib_header.header_size = little_endian_32(sizeof(bmp_dib_header_core));
+  dib_header.bitmap_width = width;
+  dib_header.bitmap_height = height;
+  dib_header.num_color_planes = little_endian_16(1);
+  dib_header.bits_per_pixel = little_endian_16(24);
+  os.write(reinterpret_cast<char*>(&dib_header), sizeof(dib_header));
+
+  char buf[3];
+  for (size_t y = 0; y < height; ++y) {
+    for (size_t x = 0; x < width; ++x) {
+      char scaled = (char)(map[(height - y - 1) * width + x] *
+                           255);  // Flip y to have same orientation as console
+                                  // debug output
+
+      buf[0] = 255 - scaled;
+      buf[1] = 255;
+      buf[2] = 255 - scaled;
+      os.write(buf, 3);
+    }
+
+    buf[0] = 0;
+    buf[1] = 0;
+    buf[2] = 0;
+    os.write(buf, row_size - 3 * width);
+  }
+}
+
 void debug_save_state(Map& map, Robot& robot,
                       const std::vector<Configuration>& configurations,
                       size_t max_width, size_t max_height,
@@ -81,47 +123,8 @@ void debug_save_state(Map& map, Robot& robot,
   size_t img_height;
   map.get_data(data.get(), max_width, max_height, &img_width, &img_height);
 
-  const size_t row_size = ((img_width * 3 - 1) / 4 + 1) * 4;
-
   std::ofstream out(path, std::ios::binary | std::ios::out);
-
-  bmp_file_header file_header;
-  file_header.header_field = little_endian_16(0x4D42);
-  file_header.file_size =
-      little_endian_32(sizeof(bmp_file_header) + sizeof(bmp_dib_header_core) +
-                       img_height * row_size);
-  file_header.reserved_1 = 0;
-  file_header.reserved_2 = 0;
-  file_header.data_offset =
-      little_endian_32(sizeof(bmp_file_header) + sizeof(bmp_dib_header_core));
-  out.write(reinterpret_cast<char*>(&file_header), sizeof(file_header));
-
-  bmp_dib_header_core dib_header;
-  dib_header.header_size = little_endian_32(sizeof(bmp_dib_header_core));
-  dib_header.bitmap_width = img_width;
-  dib_header.bitmap_height = img_height;
-  dib_header.num_color_planes = little_endian_16(1);
-  dib_header.bits_per_pixel = little_endian_16(24);
-  out.write(reinterpret_cast<char*>(&dib_header), sizeof(dib_header));
-
-  char buf[3];
-  for (size_t y = 0; y < img_height; ++y) {
-    for (size_t x = 0; x < img_width; ++x) {
-      char scaled = (char)(data.get()[(img_height - y - 1) * img_width + x] *
-                           255);  // Flip y to have same orientation as console
-                                  // debug output
-
-      buf[0] = 255 - scaled;
-      buf[1] = 255;
-      buf[2] = 255 - scaled;
-      out.write(buf, 3);
-    }
-
-    buf[0] = 0;
-    buf[1] = 0;
-    buf[2] = 0;
-    out.write(buf, row_size - 3 * img_width);
-  }
+  write_bmp(out, data.get(), img_width, img_height);
 
   out.close();
   if (!out) {
