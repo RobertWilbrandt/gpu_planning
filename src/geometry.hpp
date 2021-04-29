9fdfd0a645b9f02bf79ab50692496e392bef6a55
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-04-29 18:07:12 +0200

    Add generic pose class to geometry

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -36,6 +36,16 @@ __host__ __device__ Position<T> operator-(Position<T> p, Vector<T> v);
 template <typename T>
 __host__ __device__ Vector<T> operator-(Position<T> p1, Position<T> p2);
 
+template <typename T>
+struct Pose {
+  __host__ __device__ Pose();
+  __host__ __device__ Pose(Position<T> p, float theta);
+  __host__ __device__ Pose(T x, T y, float theta);
+
+  Position<T> position;
+  float theta;
+};
+
 /*
  * Template implementations
  */
@@ -81,5 +91,16 @@ template <typename T>
 __host__ __device__ Vector<T> operator-(Position<T> p1, Position<T> p2) {
   return Vector<T>(p1.x - p2.x, p1.y - p2.y);
 }
+
+template <typename T>
+__host__ __device__ Pose<T>::Pose() : position{0, 0}, theta{0.f} {}
+
+template <typename T>
+__host__ __device__ Pose<T>::Pose(Position<T> p, float theta)
+    : position{p}, theta{theta} {}
+
+template <typename T>
+__host__ __device__ Pose<T>::Pose(T x, T y, float theta)
+    : position{x, y}, theta{theta} {}
 
 }  // namespace gpu_planning
