56564c491de3e50255f9aa57861871253a447930
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-06-04 23:06:50 +0200

    Add cmake trace target and unify output directory

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 cmake --build . --target doc
 
 # Create baseline profile with LTTngtrace information
-lttng create baseline
+lttng create -o /build/trace/baseline
 lttng enable-event --userspace gpu_planning:*
 lttng start
 nvprof /build/gpu_planning -v |& tee /baseline.txt
